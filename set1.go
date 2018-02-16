38861a67bbd91ee6ea518019f5f82777758f004c
Travis Cline <travis.cline@gmail.com>
2018-02-16 16:50:10 -0500

    set1: implement 6 and 7

--- set1.go
+++ set1.go
@@ -2,10 +2,14 @@ package cryptopals
 
 import (
 	"bytes"
+	"crypto/aes"
 	"encoding/base64"
 	"encoding/hex"
-	"errors"
 	"fmt"
+	"math/bits"
+	"sort"
+
+	"github.com/pkg/errors"
 )
 
 var (
@@ -47,4 +51,103 @@ func XORHexSlices(a, b []byte) ([]byte, error) {
 		result[i] = da[i] ^ db[i]
 	}
 	return []byte(fmt.Sprintf("%x", result)), nil
+}
+
+// RepeatingXOR returns the hex-encoded result of repeated XOR application of key to plaintext.
+func RepeatingXOR(plaintext, key []byte) ([]byte, error) {
+	c := make([]byte, len(plaintext))
+	for i := range plaintext {
+		c[i] = plaintext[i] ^ key[i%len(key)]
+	}
+	return []byte(hex.EncodeToString(c)), nil
+}
+
+// HammingDistance computes the number of bits that differ between a and b.
+func HammingDistance(a, b []byte) (int, error) {
+	n := 0
+	if len(a) != len(b) {
+		return 0, ErrMismatchedLengths
+	}
+
+	for i := range a {
+		d := a[i] ^ b[i]
+		n += bits.OnesCount(uint(d))
+	}
+	return n, nil
+}
+
+// SolveSingleByteXOR attempts to recover plaintext from a single-byte xor encrypted ciphertext.
+func SolveSingleByteXOR(in []byte) (key byte, plaintext string, err error) {
+	type option struct {
+		key       byte
+		plaintext string
+	}
+	var options []option
+	for i := 0; i < 255; i++ {
+		x := []byte(fmt.Sprintf("%x", bytes.Repeat([]byte{byte(i)}, hex.DecodedLen(len(in)))))
+		o, err := XORHexSlices(in, x)
+		if err != nil {
+			return 0, "", errors.Wrap(err, "issue xoring slices")
+		}
+		d := make([]byte, hex.DecodedLen(len(in)))
+		_, err = hex.Decode(d, o)
+		if err != nil {
+			return 0, "", errors.Wrap(err, "issue decoding hex")
+		}
+		options = append(options, option{byte(i), string(d)})
+	}
+	sort.SliceStable(options, func(i, j int) bool {
+		return NTopEnglish(options[i].plaintext) > NTopEnglish(options[j].plaintext)
+	})
+	if len(options) == 0 {
+		return 0, "", ErrEmpty
+	}
+	/*
+		for _, o := range options[:5] {
+			fmt.Printf("%v - %v - %q\n", PercentCommonEnglish(o.plaintext), NTopEnglish(o.plaintext), o.plaintext)
+		}
+	*/
+	return options[0].key, options[0].plaintext, nil
+}
+
+// SolveRepeatingXOR attempts to recover plaintext from a given repeated-xor encrypted ciphertext and given key size.
+func SolveRepeatingXOR(ciphertext []byte, keysize int) ([]byte, error) {
+	transposedBlocks := make([][]byte, keysize)
+	for i := 0; i < len(ciphertext); i++ {
+		j := i % keysize
+		transposedBlocks[j] = append(transposedBlocks[j], ciphertext[i])
+	}
+	var key []byte
+	for _, block := range transposedBlocks {
+		blockHex := []byte(hex.EncodeToString(block))
+		k, _, err := SolveSingleByteXOR(blockHex)
+		if err != nil {
+			return nil, err
+		}
+		key = append(key, k)
+	}
+	decrypted, err := RepeatingXOR(ciphertext, key)
+	if err != nil {
+		return nil, err
+	}
+	plaintext := make([]byte, hex.DecodedLen(len(decrypted)))
+	if _, err := hex.Decode(plaintext, decrypted); err != nil {
+		return nil, err
+	}
+	return plaintext, nil
+}
+
+// DecryptAESECB decrypts the given ciphertext with the given key in ECB mode.
+func DecryptAESECB(ciphertext, key []byte) ([]byte, error) {
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: ensure len is multiple of blocksize
+	plaintext := make([]byte, len(ciphertext))
+	n := len(ciphertext) / c.BlockSize()
+	for i := 0; i < n; i++ {
+		c.Decrypt(plaintext[i*c.BlockSize():], ciphertext[i*c.BlockSize():])
+	}
+	return plaintext, nil
 }
--- set1_test.go
+++ set1_test.go
@@ -3,12 +3,13 @@ package cryptopals
 import (
 	"bufio"
 	"bytes"
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"sort"
 	"testing"
-	"unicode"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
@@ -79,33 +80,12 @@ func TestXORHexSlices(t *testing.T) {
 
 func TestChallenge3(t *testing.T) {
 	in := []byte("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-	var options []string
-	for i := 0; i < 255; i++ {
-		x := []byte(fmt.Sprintf("%x", bytes.Repeat([]byte{byte(i)}, hex.DecodedLen(len(in)))))
-		o, err := XORHexSlices(in, x)
-		if err != nil {
-			continue
-		}
-		d := make([]byte, hex.DecodedLen(len(in)))
-		_, err = hex.Decode(d, o)
-		if err != nil {
-			continue
-		}
-		unPrintable := false
-		for _, c := range string(d) {
-			if !unicode.IsPrint(c) {
-				unPrintable = true
-			}
-		}
-		if !unPrintable {
-			options = append(options, string(d))
-		}
+	key, plaintext, err := SolveSingleByteXOR(in)
+	if err != nil {
+		t.Error(err)
 	}
-	sort.SliceStable(options, func(i, j int) bool {
-		return NTopEnglish(options[i]) > NTopEnglish(options[j])
-	})
 	// print string with highest score
-	t.Logf("%v %v", NTopEnglish(options[0]), options[0])
+	t.Logf("%v %v %v", key, NTopEnglish(plaintext), plaintext)
 }
 
 func TestChallenge4(t *testing.T) {
@@ -156,4 +136,158 @@ func TestChallenge4(t *testing.T) {
 			t.Logf("%v %v %v %v %q\n", NTopEnglish(s), PercentLetters(s), PercentCommonEnglish(s), len(s), s)
 		}
 	*/
+}
+
+func TestRepeatingXOR(t *testing.T) {
+	type args struct {
+		plaintext []byte
+		key       []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{"nils", args{nil, nil}, nil, false},
+		{"example", args{[]byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"), []byte("ICE")},
+			[]byte("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"),
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := RepeatingXOR(tt.args.plaintext, tt.args.key)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RepeatingXOR() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !cmp.Equal(got, tt.want, cmpopts.EquateEmpty()) {
+				t.Errorf("RepeatingXOR() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHammingDistance(t *testing.T) {
+	type args struct {
+		a []byte
+		b []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{"nils", args{nil, nil}, 0, false},
+		{"one nil", args{nil, []byte("x")}, 0, true},
+		{"zero", args{[]byte("a"), []byte("a")}, 0, false},
+		{"example", args{[]byte("this is a test"), []byte("wokka wokka!!!")}, 37, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := HammingDistance(tt.args.a, tt.args.b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HammingDistance() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("HammingDistance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+type KeyDist struct {
+	N int
+	D float64
+}
+
+func RepeatingXORKeySizes(ciphertext []byte) ([]KeyDist, error) {
+	var distances []KeyDist
+	// TODO: be flexible in size
+	for keysize := 2; keysize < 40; keysize++ {
+		bufA := ciphertext[:keysize]
+		bufB := ciphertext[keysize : 2*keysize]
+		bufC := ciphertext[2*keysize : 3*keysize]
+		bufD := ciphertext[3*keysize : 4*keysize]
+		//spew.Dump(bufA, bufB, bufC, bufD, ciphertext)
+		hd1, err := HammingDistance(bufA, bufB)
+		if err != nil {
+			return nil, err
+		}
+		hd2, err := HammingDistance(bufC, bufD)
+		if err != nil {
+			return nil, err
+		}
+		hd3, err := HammingDistance(bufA, bufC)
+		if err != nil {
+			return nil, err
+		}
+		hd4, err := HammingDistance(bufB, bufD)
+		if err != nil {
+			return nil, err
+		}
+		hd := (hd1 + hd2 + hd3 + hd4) / 4.0
+		d := float64(hd) / float64(keysize)
+		distances = append(distances, KeyDist{keysize, d})
+	}
+	sort.SliceStable(distances, func(i, j int) bool {
+		return distances[i].D < distances[j].D
+	})
+	return distances, nil
+}
+
+func GuessXORKeySize(ciphertext []byte) (int, error) {
+	sizes, err := RepeatingXORKeySizes(ciphertext)
+	if err != nil {
+		return 0, err
+	}
+	return sizes[0].N, nil
+}
+
+func TestChallenge6(t *testing.T) {
+	encoded, err := ioutil.ReadFile("6.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	contents := make([]byte, base64.StdEncoding.DecodedLen(len(encoded)))
+	// un-base64
+	if _, err := base64.StdEncoding.Decode(contents, encoded); err != nil {
+		t.Fatal(err)
+	}
+	keysizes, err := RepeatingXORKeySizes(contents)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var results []string
+	// check top 5 key sizes
+	for _, keysize := range keysizes[:5] {
+		t.Logf("trying keysize=%v", keysize)
+		plaintext, err := SolveRepeatingXOR(contents, keysize.N)
+		if err != nil {
+			t.Error(err)
+		}
+		results = append(results, string(plaintext))
+	}
+
+	sort.SliceStable(results, func(i, j int) bool {
+		return NTopEnglish(results[i]) > NTopEnglish(results[j])
+	})
+	t.Logf("%v", results[0])
+}
+
+func TestChallenge7(t *testing.T) {
+	encoded, err := ioutil.ReadFile("7.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	contents := make([]byte, base64.StdEncoding.DecodedLen(len(encoded)))
+	if _, err := base64.StdEncoding.Decode(contents, encoded); err != nil {
+		t.Fatal(err)
+	}
+	plaintext, err := DecryptAESECB(contents, []byte("YELLOW SUBMARINE"))
+	t.Logf("%q", plaintext)
 }
